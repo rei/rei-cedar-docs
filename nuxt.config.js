f607f30a0db795351240b2a180e6200ba726d120
Allen Taylor <allen.taylor@paviasystems.com>
2018-02-06 17:26:01 -0800

    Adding markdownit module options to NUXT configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,15 +3,28 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'nuxt-demo',
+    title: 'rei-cedar-docs',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Cedar Documentation Site' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
+  },
+  /*
+  * Extend core functionality
+  */
+  modules: [
+    '@nuxtjs/markdownit'
+  ],
+  /*
+  * markdownit options
+  * See https://github.com/markdown-it/markdown-it
+  */
+  markdownit: {
+    preset: 'default',
   },
   /*
   ** Customize the progress bar color
