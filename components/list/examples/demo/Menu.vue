d03fc2bd658619430bda692fb962f22847714234
Unknown <allen.taylor@paviasystems.com>
2018-02-15 14:33:15 -0800

    Adding vue files for the examples sections of each component

---
+++ components/list/examples/demo/Menu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>Menu List</h3>
+
+    <cdr-list
+      modifier="menu"
+      data-backstop="cdr-list--menu"
+    >
+      <li><a href="/">List menu</a></li>
+      <li><a href="/">List menu</a></li>
+    </cdr-list>
+  </div>
+</template>
+
+<script>
+import Components from 'Components/_index';
+
+export default {
+  name: 'MenuList',
+  components: Components,
+};
+</script>
+
+<style>
+
+</style>
---
+++ components/list/examples/demo/Ordered.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h3>Ordered List</h3>
+    <cdr-list list-type="ol">
+      <li>List ordered</li>
+      <li>List ordered</li>
+      <li>List ordered</li>
+    </cdr-list>
+
+    <cdr-list
+      list-type="ol"
+      modifier="compact">
+      <li>List ordered - compact</li>
+      <li>List ordered - compact</li>
+      <li>List ordered - compact</li>
+    </cdr-list>
+
+    <cdr-list
+      list-type="ol"
+      modifier="spread">
+      <li>List ordered - spread</li>
+      <li>List ordered - spread</li>
+      <li>List ordered - spread</li>
+    </cdr-list>
+
+    <cdr-list list-type="ol">
+      <li>List ordered Nested
+        <cdr-list list-type="ol">
+          <li>List ordered - nested</li>
+          <li>List ordered - nested
+            <cdr-list
+              list-type="ol"
+              modifier="numbered">
+              <li>List ordered - nested - numbered</li>
+              <li>List ordered - nested - numbered</li>
+            </cdr-list>
+          </li>
+        </cdr-list>
+      </li>
+    </cdr-list>
+  </div>
+</template>
+
+<script>
+import Components from 'Components/_index';
+
+export default {
+  name: 'OrderedList',
+  components: Components,
+};
+</script>
+
+<style>
+
+</style>
