453b6fc4a44d23d9d540a8e9f26d842fc5b56793
Allen Taylor <allen.taylor@paviasystems.com>
2018-02-15 15:53:58 -0800

    Adding card cedar component

---
+++ components/card/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div v-html="cardMD"></div>
+    <Cards/>
+  </div>
+</template>
+
+<script>
+import cardMD from '~/components/card/archive/cdrCard.md'
+import Cards from '~/components/card/examples/Cards.vue'
+
+export default {
+  name: 'Card',
+  components: {
+    Cards
+  },
+  computed: {
+    cardMD () {
+      return cardMD
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div>
     <Anchor/>
     <Button/>
+    <Card/>
   </div>
 </template>
 
@@ -10,12 +11,13 @@ import '@rei/cdr-assets/dist/cdr-core.css'
 
 import Anchor from '~/components/anchor/Anchor.vue'
 import Button from '~/components/button/Button.vue'
-
+import Card from '~/components/card/Card.vue'
 
 export default {
   components: {
     Anchor,
-    Button
+    Button,
+    Card
   }
 }
 </script>
