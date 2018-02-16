9ef54f5f6ad69104408eacf726c4b22969c2c6fb
Allen Taylor <allen.taylor@paviasystems.com>
2018-02-15 16:19:34 -0800

    Added checkbox component

---
+++ components/checkbox/Checkbox.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div v-html="checkboxMD"></div>
+    <Checkboxes/>
+  </div>
+</template>
+
+<script>
+import checkboxMD from '~/components/checkbox/archive/cdrCheckbox.md'
+import Checkboxes from '~/components/checkbox/examples/checkboxes.vue'
+
+import '@rei/cdr-checkbox/dist/cdr-checkbox.css'
+
+export default {
+  name: 'Checkbox',
+  components: {
+    Checkboxes
+  }
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,21 +3,25 @@
     <Anchor/>
     <Button/>
     <Card/>
+    <Checkbox/>
   </div>
 </template>
 
 <script>
-import '@rei/cdr-assets/dist/cdr-core.css'
-
 import Anchor from '~/components/anchor/Anchor.vue'
 import Button from '~/components/button/Button.vue'
 import Card from '~/components/card/Card.vue'
+import Checkbox from '~/components/checkbox/Checkbox.vue'
+
+import '@rei/cdr-assets/dist/cdr-core.css'
+
 
 export default {
   components: {
     Anchor,
     Button,
-    Card
+    Card,
+    Checkbox
   }
 }
 </script>
