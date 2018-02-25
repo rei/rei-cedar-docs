80c123d39dccb1065a1287ecd94884717bfe18b7
Allen Taylor <allen.taylor@paviasystems.com>
2018-02-25 14:44:41 -0800

    Adding Versions files to show archived versions

---
+++ components/Versions.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-if="getVersions.length > 0">
+    <ul>
+      <li v-for="version in getVersions" :key="version">
+        <a :href="'#'">{{version}}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import versions from '~/components/_versions'
+
+export default {
+  name: 'Versions',
+  props: {
+    compPath: {
+      type: String,
+      require: true,
+    }
+  },
+  computed: {
+    getVersions() {
+      return versions(compPath)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
