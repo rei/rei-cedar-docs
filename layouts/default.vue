5fab25446c46a61c759c755858a3ddfd10a12f5f
Allen Taylor <allen.taylor@paviasystems.com>
2018-03-14 12:46:36 -0700

    Refactored the styling for the API doc site.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,34 +1,36 @@
 <template>
   <div>
-    <aside>
-      <div class="title">
-        <cdr-heading level="1">REI Cedar Style Guide</cdr-heading>
-      </div>
-      <nav>
-        <cdr-search @input="updateSearch" bare></cdr-search>
-        <cdr-list modifier="unstyled">
-          <transition name="list" move-class="list">
-            <li v-if="filterNavGroup(componentNames)"><a href="#components">Components</a></li>
-          </transition>
-          <transition-group class="cdr-list cdr-list--unstyled" move-class="list" name="list" tag="ul">
-            <li v-for="name in componentNames" :key="name" v-if="filterNavItem(name)">
-              <nuxt-link :to="'/' + name">{{name}}</nuxt-link>
-            </li>
-          </transition-group>
-          <transition name="list" move-class="list">
-            <li v-if="filterNavGroup(compositionNames)"><a href="#compositions">Compositions</a></li>
-          </transition>
-          <transition-group class="cdr-list cdr-list--unstyled" move-class="list" name="list" tag="ul">
-            <li v-for="name in compositionNames" :key="name" v-if="filterNavItem(name)">
-              <nuxt-link :to="'/' + name">{{name}}</nuxt-link>
-            </li>
-          </transition-group>
-        </cdr-list>
-      </nav>
-    </aside>
     <transition>
-      <div class="content">
-        <nuxt/>
+      <div class="container">
+        <main>
+          <nuxt/>
+        </main>
+        <aside>
+          <div class="title">
+            <cdr-heading level="1">REI Cedar Style Guide</cdr-heading>
+          </div>
+          <nav>
+            <cdr-search @input="updateSearch" bare></cdr-search>
+            <cdr-list modifier="unstyled">
+              <transition name="list" move-class="list">
+                <li v-if="filterNavGroup(componentNames)"><a href="#components">Components</a></li>
+              </transition>
+              <transition-group class="cdr-list cdr-list--unstyled" move-class="list" name="list" tag="ul">
+                <li v-for="name in componentNames" :key="name" v-if="filterNavItem(name)">
+                  <nuxt-link :to="'/' + name">{{name}}</nuxt-link>
+                </li>
+              </transition-group>
+              <transition name="list" move-class="list">
+                <li v-if="filterNavGroup(compositionNames)"><a href="#compositions">Compositions</a></li>
+              </transition>
+              <transition-group class="cdr-list cdr-list--unstyled" move-class="list" name="list" tag="ul">
+                <li v-for="name in compositionNames" :key="name" v-if="filterNavItem(name)">
+                  <nuxt-link :to="'/' + name">{{name}}</nuxt-link>
+                </li>
+              </transition-group>
+            </cdr-list>
+          </nav>
+        </aside>
       </div>
     </transition>
   </div>
