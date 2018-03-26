026ec4e69bef934b795539ad0b7ce967944c74bc
Unknown <allen.taylor@paviasystems.com>
2018-03-26 10:56:24 -0700

    Added cedar column live example

--- pages/CdrCol.vue
+++ pages/CdrCol.vue
@@ -2,23 +2,56 @@
   <div>
     <Versions comp-path="CdrCol"/>
     <nuxt-child/>
-    <Columns/>
+    <vuep :scope="scope" :value="value"></vuep>
   </div>
 </template>
 
 <script>
 import Versions from '~/components/Versions.vue'
-import Columns from '~/components/column/examples/Columns.vue'
+import Components from '~/components/_index'
+
+const CdrCol = Components.CdrCol
+const CdrRow = Components.CdrRow
 
 export default {
   name: 'Column',
   components: {
-    Columns,
     Versions
+  },
+  data() {
+    return {
+      scope: { CdrCol, CdrRow },
+      value: `
+<template>
+  <div>
+    <h3>Basic Column</h3>
+    <cdr-row>
+      <cdr-col span="3">
+        One
+      </cdr-col>
+      <cdr-col span="3">
+        Two
+      </cdr-col>
+      <cdr-col span="3">
+        Three
+      </cdr-col>
+      <cdr-col span="3">
+        Four
+      </cdr-col>
+    </cdr-row>
+  </div>
+</template>
+
+<script>
+  module.exports = {
+    name: 'live-example',
+    components: {
+      CdrCol,
+      CdrRow
+    }
+  }
+<\/script>`
+    }
   }
 }
 </script>
-
-<style>
-
-</style>
