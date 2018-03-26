cfa248ff748cd3279d80a21556ac40e377711fb9
Unknown <allen.taylor@paviasystems.com>
2018-03-26 16:57:49 -0700

    Added live code example for cedar search composition

--- pages/CdrSearch.vue
+++ pages/CdrSearch.vue
@@ -2,6 +2,7 @@
   <div>
     <Versions comp-path="CdrSearch"/>
     <nuxt-child/>
+    <vuep :scope="scope" :value="value"></vuep>
     <Searches/>
   </div>
 </template>
@@ -9,14 +10,44 @@
 <script>
 import Versions from '~/components/Versions.vue'
 import Searches from '~/compositions/search/examples/searchbox.vue'
+import Compositions from '~/compositions/_index'
 
 import '@rei/cdr-search/dist/cdr-search.css'
+
+const CdrSearch = Compositions.CdrSearch
 
 export default {
   name: 'Search',
   components: { 
     Searches,
     Versions 
+  },
+  data() {
+    return {
+      scope: { CdrSearch },
+      value: `
+<template>
+  <div>
+    <cdr-search
+    v-model="search1"/>
+    <p>Searching: {{ search1 }}</p>
+  </div>
+</template>
+
+<script>
+  module.exports = {
+    name: 'live-example',
+    components: {
+      CdrSearch
+    },
+    data: function() {
+      return {
+        search1: ''
+      }
+    }
+  }
+<\/script>`
+    }
   }
 }
 </script>
