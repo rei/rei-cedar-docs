a7355b6aa658a8d0a57ad3e4ac0d50679eef2b07
Unknown <allen.taylor@paviasystems.com>
2018-03-26 11:24:13 -0700

    Added cedar heading live code example

--- pages/CdrHeading.vue
+++ pages/CdrHeading.vue
@@ -2,25 +2,54 @@
   <div>
     <Versions comp-path="CdrHeading"/>
     <nuxt-child/>
-    <Headings/>
+    <vuep :scope="scope" :value="value"></vuep>
   </div>
 </template>
 
 <script>
 import Versions from '~/components/Versions.vue'
-import Headings from '~/components/heading/examples/Headings.vue'
+import Components from '~/components/_index'
 
 import '@rei/cdr-heading/dist/cdr-heading.css'
+
+const CdrHeading = Components.CdrHeading
 
 export default {
   name: 'Heading',
   components: {
-    Headings,
     Versions
   },
+  data() {
+    return {
+      scope: { CdrHeading },
+      value: `
+<template>
+  <div>
+    <cdr-heading
+      level="1"
+      modifier="spruce-dsp-70">spruce-dsp-70</cdr-heading>
+    <cdr-heading
+        level="4"
+        modifier="spruce-dsp-60">spruce-dsp-60</cdr-heading>
+    <cdr-heading
+      level="5"
+      modifier="spruce-dsp-50">spruce-dsp-50</cdr-heading>
+    <cdr-heading
+      level="6"
+      modifier="spruce-dsp-40">spruce-dsp-40</cdr-heading>
+  </div>
+</template>
+
+<script>
+  module.exports = {
+    name: 'live-example',
+    components: {
+      CdrHeading
+    }
+  }
+<\/script>
+`
+    }
+  }
 }
 </script>
-
-<style>
-
-</style>
