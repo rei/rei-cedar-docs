b3b8a3db4e4a698a606ba18b44145fcfae21af53
Unknown <allen.taylor@paviasystems.com>
2018-03-28 12:52:36 -0700

    Correcting the references to package.json files for compositions and components

--- compositions/_index.js
+++ compositions/_index.js
@@ -2,7 +2,7 @@ import { CdrActivityCard } from '@rei/cdr-activity-card'
 import { CdrSearch } from '@rei/cdr-search'
 
 import cdrActivityCardPkg from '@rei/cdr-activity-card/package.json'
-import cdrSearchPkg from '@rei/cdr-search-package.json'
+import cdrSearchPkg from '@rei/cdr-search/package.json'
 
 export default {
   CdrActivityCard,
