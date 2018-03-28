e7c91ad36d8110e49037a9ccf3204f8428534c3b
Allen Taylor <allen.taylor@paviasystems.com>
2018-03-28 10:58:20 -0700

    Added imports for the `package.json` of cedar components and compositions

--- components/_index.js
+++ components/_index.js
@@ -14,6 +14,21 @@ import { CdrRating } from '@rei/cdr-rating';
 import { CdrRow } from '@rei/cdr-row';
 import { CdrSelect } from '@rei/cdr-select';
 
+import cdrAPkg from '@rei/cdr-a/package.json'
+import cdrButtonPkg from '@rei/cdr-button/package.json'
+import cdrCardPkg from '@rei/cdr-card/package.json'
+import cdrCheckboxPkg from '@rei/cdr-checkbox/package.json'
+import cdrColPkg from '@rei/cdr-col/package.json'
+import cdrHeadingPkg from '@rei/cdr-heading/package.json'
+import cdrIconPkg from '@rei/cdr-icon/package.json'
+import cdrImgPkg from '@rei/cdr-img/package.json'
+import cdrListPkg from '@rei/cdr-list/package.json'
+import cdrMediaObjectPkg from '@rei/cdr-media-object/packag.json'
+import cdrRadioPkg from '@rei/cdr-radio/package.json'
+import cdrRatingPkg from '@rei/cdr-rating/package.json'
+import cdrRowPkg from '@rei/cdr-row/package.json'
+import cdrSelectPkg from '@rei/cdr-select/package.json'
+
 export default {
   CdrA,
   CdrButton,
@@ -31,3 +46,21 @@ export default {
   CdrRow,
   CdrSelect,
 };
+
+const cdrComponentPkgs = {
+  cdrAPkg,
+  cdrButtonPkg,
+  cdrCardPkg,
+  cdrCheckboxPkg,
+  cdrColPkg,
+  cdrHeadingPkg,
+  cdrIconPkg,
+  cdrImgPkg,
+  cdrListPkg,
+  cdrMediaObjectPkg,
+  cdrRadioPkg,
+  cdrRatingPkg,
+  cdrRowPkg,
+  cdrSelectPkg
+}
+export { cdrComponentPkgs };
--- compositions/_index.js
+++ compositions/_index.js
@@ -1,7 +1,17 @@
 import { CdrActivityCard } from '@rei/cdr-activity-card'
 import { CdrSearch } from '@rei/cdr-search'
 
+import cdrActivityCardPkg from '@rei/cdr-activity-card/package.json'
+import cdrSearchPkg from '@rei/cdr-search-package.json'
+
 export default {
   CdrActivityCard,
   CdrSearch
 }
+
+const cdrCompositionPkgs = {
+  cdrActivityCardPkg,
+  cdrSearchPkg
+}
+
+export { cdrCompositionPkgs }
