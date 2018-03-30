2c0bd5ba5bd7a6919ef503e921e9d669632998de
Unknown <allen.taylor@paviasystems.com>
2018-03-30 10:57:15 -0700

    Adding status icons for each of the components

--- pages/Introduction.vue
+++ pages/Introduction.vue
@@ -2,6 +2,23 @@
   <div>
     <h1 id="intro">Introduction</h1>
     <div v-html="introMD"></div>
+    <cdr-list modifier="inline unstyled">
+      <li>
+        <cdr-icon :url="`${svg}#cdr-check-circ-stroke`"></cdr-icon>&nbsp;<span>Ready</span>
+      </li>
+      <li>
+        <cdr-icon :url="`${svg}#cdr-warning-tri`"></cdr-icon>&nbsp;<span>Under Review</span>
+      </li>
+      <li>
+        <cdr-icon :url="`${svg}#cdr-x-circ-stroke`"></cdr-icon>&nbsp;<span>Deprecated</span>
+      </li>
+      <li>
+        <cdr-icon :url="`${svg}#cdr-check-service`"></cdr-icon>&nbsp;<span>Prototype</span>
+      </li>
+      <li>
+        <cdr-icon :url="`${svg}#cdr-minus-sm`"></cdr-icon>&nbsp;<span>Not Applicable</span>
+      </li>
+    </cdr-list>
     <cdr-row v-for="name in componentNames" :key="name">
       <cdr-col> {{ name }} </cdr-col>
       <cdr-col> {{ namedComponentPkg(name, 'version') }} </cdr-col>
@@ -13,11 +30,14 @@
 import introMD from '~/old_docs/Introduction.md'
 import Components, { cdrComponentPkgs } from '~/components/_index'
 import Compositions, { cdrCompositionPkgs } from '~/compositions/_index'
+import svg from '~/assets/rei-icons.svg'
 import compNamesMixin from '~/mixins/comp-names'
 import util from 'util'
 
 const CdrRow = Components.CdrRow
+const CdrList = Components.CdrList
 const CdrCol = Components.CdrCol
+const CdrIcon = Components.CdrIcon
 
 export default {
   mixins: [compNamesMixin],
@@ -25,12 +45,15 @@ export default {
     return {
       cdrComponentPkgs,
       cdrCompositionPkgs,
-      introMD
+      introMD,
+      svg
     }
   },
   components: {
     CdrRow,
-    CdrCol
+    CdrCol,
+    CdrList,
+    CdrIcon
   },
   computed: {
     componentNames () {
