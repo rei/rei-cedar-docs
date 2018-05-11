cf7b88ccca08eddbe891dd779bc19a8208f777b7
Kevin Powell <kevin@eightshapes.com>
2018-05-11 14:19:33 -0500

    Add Hero Doc Component

--- pages/DocComponentProvingGround.vue
+++ pages/DocComponentProvingGround.vue
@@ -2,16 +2,91 @@
   <div>
     <cdr-doc-alert/>
     <cdr-doc-alert>Some custom alert message here</cdr-doc-alert>
+    <cdr-doc-hero/>
+    <!-- Leaving some space to separate them on the proving ground  -->
+    <br>
+    <cdr-doc-hero 
+      title="Link" 
+      :metadata="false">
+      Links are used for navigation between pages.
+    </cdr-doc-hero>
+    <!-- Leaving some space to separate them on the proving ground  -->
+    <br>
+    <cdr-doc-hero 
+      title="No Metadata and No Breadcrumbs" 
+      :metadata="false" 
+      :breadcrumbs="false">
+      Metadata and breadcrumb options can be disabled by passing a 'false' value.
+    </cdr-doc-hero>
+    <!-- Leaving some space to separate them on the proving ground  -->
+    <br>
+    <cdr-doc-hero
+      :breadcrumbs="[
+        {
+          text: 'Breadcrumb 1',
+          href: '#'
+        },
+        {
+          text: 'Breadcrumb 2',
+          href: '#'
+        },
+        {
+          text: 'Breadcrumb 3',
+          href: '#'
+        },
+        {
+          text: 'Breadcrumb 4',
+          href: '#'
+        },
+        {
+          text: 'Breadcrumb 5',
+          href: '#'
+        },
+        {
+          text: 'Breadcrumb 6',
+          href: '#'
+        },
+        {
+          text: 'Breadcrumb 7',
+          href: '#'
+        },
+        {
+          text: 'Breadcrumb 8',
+          href: '#'
+        },
+        {
+          text: 'Breadcrumb 9',
+          href: '#'
+        } ,
+        {
+          text: 'Breadcrumb 10',
+          href: '#'
+        } ,
+        {
+          text: 'Breadcrumb 11',
+          href: '#'
+        } ,
+        {
+          text: 'Breadcrumb 12',
+          href: '#'
+        } 
+      ]"
+      title="Content Resilience Test No Component Name Should Ever Be This Long Or We Have Bigger Problems To Worry About"
+      metadata="Really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really long metadata">
+      This is where the summary goes. Also really really really really really really really really really really really really really really really really really really really really long.
+    </cdr-doc-hero>
   </div>
 </template>
 
 <script>
 import CdrDocAlert from '~/src/doc-components/alert/Alert'
+import CdrDocHero from '~/src/doc-components/hero/Hero'
 
 export default {
   layout: 'proving-ground',
   components: {
-    CdrDocAlert
+    CdrDocAlert,
+    CdrDocHero
   }
 }
 </script>
---
+++ src/doc-components/hero/Hero.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="cdr-doc-hero">
+    <div class="cdr-doc-hero__inner">
+        <ul class="cdr-doc-hero__breadcrumbs" v-if="breadcrumbs">
+          <li v-for="breadcrumb in breadcrumbs">
+            <a :href="breadcrumb.href" class="cdr-doc-hero__breadcrumb-link">{{ breadcrumb.text }}</a>
+          </li>
+        </ul>
+        <h1 class="cdr-doc-hero__title">{{ title }}</h1>
+        <h2 class="cdr-doc-hero__title-metadata" v-if="metadata">{{ metadata }}</h2>
+        <p class="cdr-doc-hero__summary">
+          <slot>
+            Buttons are used to invoke an event and communicate the action that will occur.
+          </slot>
+        </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CdrDocHero',
+  props: {
+    breadcrumbs: {
+      type: Array,
+      default: [
+        {
+          text: 'Components/',
+          href: '#'
+        }
+      ]
+    },
+    title: {
+      type: String,
+      default: 'Button'
+    },
+    metadata: {
+      type: String,
+      default: 'Also known as a Call-to-Action (CTA)'
+    }
+  }
+}
+</script>
+<style lang="scss">
+  @import '../../../assets/css/cedar-tokens.scss';
+  $background-color-hero: $crimp-son-and-clover;
+  $text-color-hero: $clean-slate;
+  $content-max-width: 900px;
+
+  .cdr-doc-hero {
+    background: $background-color-hero;
+    color: $text-color-hero;
+    padding: $space-4-x $space-1-x;
+  }
+
+  .cdr-doc-hero__inner {
+    margin: 0 auto;
+    max-width: $content-max-width;
+  }
+
+  .cdr-doc-hero__title {
+    @include spruce-display-80;
+    border-bottom: solid 1px $text-color-hero;
+    margin-bottom: $space-1-x;
+    padding-bottom: $space-1-x;
+  }
+
+  .cdr-doc-hero__breadcrumbs {
+    align-items: center;
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  .cdr-doc-hero__breadcrumb-link {
+    @include spruce-display-00;
+    color: $text-color-hero;
+    margin-right: $space-half-x;
+
+    &:hover {
+      color: $text-color-hero;
+    }
+  }
+
+  .cdr-doc-hero__title-metadata {
+    @include redwood-display-20;
+    margin-bottom: $space-half-x;
+  }
+
+  .cdr-doc-hero__summary {
+    @include redwood-display-60;
+  }
+</style>
