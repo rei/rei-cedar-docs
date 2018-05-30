238d82160b2554e7d3bd4ec5f3c706a4cbebde13
Allen Taylor <allen.taylor@paviasystems.com>
2018-05-29 17:06:39 -0700

    Added skeleton for cedar docs api component

---
+++ docs/.vuepress/components/cdr-doc-api.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class=".cdr-doc-api">
+  </div>
+</template>
+
+<script>
+  import { CdrCol } from '@rei/cdr-col'
+  import { CdrRow } from '@rei/cdr-row'
+  
+  export default {
+    name: 'CdrDocApi',
+    components: {
+      CdrCol,
+      CdrRow
+    }
+  };
+</script>
+
+<style lang="scss">
+  @import '../theme/styles/cdr-tokens.scss';
+  
+  .cdr-doc-api {
+    box-sizing: border-box;
+    height: 84px;
+    width: 689px;
+    border: 1px solid $partly-cloudy;
+    border-radius: 4px;
+    background-color: $clean-slate
+  }
+</style>
