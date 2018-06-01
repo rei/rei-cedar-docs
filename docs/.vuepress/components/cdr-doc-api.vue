b4703ace5d38ec9585082559184ecfc8bf7d60cb
Unknown <allen.taylor@paviasystems.com>
2018-06-01 11:54:16 -0700

    Add high order component to display each type of api component

---
+++ docs/.vuepress/components/cdr-doc-api.vue
@@ -0,0 +1,23 @@
+<script>
+import apiEvent from './api-event.vue';
+import apiProp from './api-prop.vue';
+import apiSlot from './api-slot.vue';
+
+export default {
+  name: 'CdrDocApi',
+  functional: true,
+  props: {
+    apiType: {
+      type: String,
+      required: true,
+      validator: value => {
+        return ['prop', 'slot', 'event'].indexOf(value) > -1;
+      }
+    }
+  },
+  render: function(createElement, context) {
+
+    return createElement()
+  }
+}
+</script>
