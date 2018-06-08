4704f5744143bccee9fd76c92dd134c14f0b01de
Kevin Powell <kevin@eightshapes.com>
2018-06-08 15:30:47 -0500

    PLIB-2509 WIP Html Example List Doc Component

---
+++ docs/.vuepress/components/cdr-doc-html-example-list.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="cdr-doc-html-example-list" v-on:click="codeToConsole">
+    <div class="cdr-doc-html-example-list-slot-wrap" ref="code">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CdrDocHtmlExampleList',
+  props: {
+
+  },
+  data: function() {
+    return {
+
+    }
+  },
+  beforeCreate: function () {
+    console.log(this.$slots.default);
+  },
+  methods: {
+    codeToConsole () {
+      console.log(this.$refs.code.querySelector('pre').textContent);
+    }
+  }
+}
+</script>
+<style lang="scss">
+  @import '../theme/styles/cdr-tokens.scss';
+  @import '../theme/styles/cdr-doc-tokens.scss';
+  $cdr-doc-code-snippet-actions-background-color: $ice-age;
+
+</style>
---
+++ docs/proving-grounds/html-example-list/README.md
@@ -0,0 +1,15 @@
+---
+navbar: false
+pageClass: cdr-doc-proving-ground
+---
+
+# Html Example List Proving Grounds
+
+## Default Component Behavior
+<cdr-button/>
+<cdr-doc-html-example-list>
+  <cdr-button size="large"/>
+  <cdr-button size="small"/>
+  <cdr-button>Foo</cdr-button>
+</cdr-doc-html-example-list>
+
