d2180a39c3df505a9e14a3a97226c7708ca39b77
Kevin Powell <kevin@eightshapes.com>
2018-06-11 15:31:36 -0500

    PLIB-2509 basic styling

--- docs/.vuepress/components/cdr-doc-html-example-list.vue
+++ docs/.vuepress/components/cdr-doc-html-example-list.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="cdr-doc-html-example-list" v-on:click="codeToConsole">
     <div class="cdr-doc-html-example-list__item" v-for="slotContent, label in $slots">
-      <h1>{{ label }}</h1>
+      <span class="cdr-doc-html-example-list__item-label">{{ label }}</span>
       <!-- Code is rendered as a string here, then extracted, compiled, and injected as an html example -->
       <div class="cdr-doc-html-example-list__code" :ref="'cdr-doc-html-example-list__code-source-' + label">
         <slot :name="label"/>
       </div>
-      <div :id="'cdr-doc-html-example-' + label">
+      <div :id="'cdr-doc-html-example-' + label" class="cdr-doc-html-example-list__item">
         <!-- Will be replaced with the compiled template code -->
       </div>
     </div>
@@ -14,7 +14,7 @@
 </template>
 
 <script>
-  import Vue from '$vue'; 
+  import Vue from '$vue';
   // Note the '$vue', which is the full build of Vue, not the default runtime 'vue'. We need to import the full build to have access to the compiler
   
   // Import components that will be shown as examples
@@ -58,7 +58,29 @@
   @import '../theme/styles/cdr-doc-tokens.scss';
   $cdr-doc-code-snippet-actions-background-color: $ice-age;
 
-  // .cdr-doc-html-example-list-slot-wrap {
-  //   display: none;
-  // }
+  .cdr-doc-html-example-list {
+    border: $cdr-doc-border-separator;
+  }
+
+  .cdr-doc-html-example-list__item {
+    align-items: center;
+    border-bottom: $cdr-doc-border-separator;
+    display: flex;
+    justify-content: center;
+    position: relative;
+
+    &:last-child {
+      border-bottom: 0;
+    }
+  }
+
+  .cdr-doc-html-example-list__code {
+    display: none;
+  }
+
+  .cdr-doc-html-example-list__item-label {
+    position: absolute;
+    right: 0;
+    top: 0;
+  }
 </style>
