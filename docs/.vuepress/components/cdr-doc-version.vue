bd927a26cd4b9d8e3b23411e5c7143d24d331e6d
Allen Taylor <allen.taylor@paviasystems.com>
2018-06-11 14:26:22 -0700

    Create skeleton for cedar docs site version component

---
+++ docs/.vuepress/components/cdr-doc-version.vue
@@ -0,0 +1,14 @@
+<template>
+  
+</template>
+
+<script>
+import { CdrSelect } from '@rei/cdr-select';
+
+export default {
+  name: 'CdrDocVersion',
+  components: {
+    CdrSelect
+  }
+};
+</script>
