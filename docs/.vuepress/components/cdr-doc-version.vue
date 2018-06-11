6cbc1f3afd49d52d27b17fed61d5786d06da9e49
Allen Taylor <allen.taylor@paviasystems.com>
2018-06-11 15:53:58 -0700

    Add props to version component to display semver versions

--- docs/.vuepress/components/cdr-doc-version.vue
+++ docs/.vuepress/components/cdr-doc-version.vue
@@ -1,5 +1,10 @@
 <template>
-  
+  <cdr-select
+    label="Version Select",
+    prompt="Select the API version",
+    :options="$page.frontmatter.versions.map(version => version['version'])"
+  >
+  </cdr-select>
 </template>
 
 <script>
