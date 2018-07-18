b2efad089cca93acc3367ee4f6457f615d0c21c3
Lance Harper <laharpe@rei.com>
2018-07-18 13:56:07 -0700

    Home page re-design layout

--- docs/.vuepress/cedar.js
+++ docs/.vuepress/cedar.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'; // for registering components globally
 import FullVue from '$vue'; // for registering components globally
 
-// Core assets
-import "@rei/cdr-assets/dist/cdr-core.css";
-import "@rei/cdr-assets/dist/cdr-fonts.css";
-
 // Components
 import { CdrButton } from "@rei/cdr-button";
 import "@rei/cdr-button/dist/cdr-button.css";
@@ -39,11 +35,18 @@ import { CdrText } from "@rei/cdr-text";
 Vue.component("CdrText", CdrText);
 FullVue.component("CdrText", CdrText);
 
-import { CdrRow } from "@rei/cdr-grid";
+import { CdrRow, CdrCol } from "@rei/cdr-grid";
+import "@rei/cdr-grid/dist/cdr-grid.css";
 Vue.component('CdrRow', CdrRow);
 FullVue.component('CdrRow', CdrRow);
-
-import { CdrCol } from "@rei/cdr-grid";
-import "@rei/cdr-grid/dist/cdr-grid.css";
 Vue.component('CdrCol', CdrCol);
 FullVue.component('CdrCol', CdrCol);
+
+import { CdrCard } from "@rei/cdr-card";
+import "@rei/cdr-card/dist/cdr-card.css";
+Vue.component('CdrCard', CdrCard);
+FullVue.component('CdrCard', CdrCard);
+
+// Core assets
+import "@rei/cdr-assets/dist/cdr-core.css";
+import "@rei/cdr-assets/dist/cdr-fonts.css";
--- docs/.vuepress/theme/Home.vue
+++ docs/.vuepress/theme/Home.vue
@@ -1,25 +1,187 @@
 <template>
   <div class="home">
-    <div class="hero">
-      <img v-if="data.heroImage" :src="$withBase(data.heroImage)" alt="hero">
-      <h1>{{ data.heroText || $title || 'Hello' }}</h1>
-      <p class="description">
-        {{ data.tagline || $description || 'Welcome to your VuePress site' }}
-      </p>
-      <p class="action" v-if="data.actionText && data.actionLink">
-        <NavLink class="action-button" :item="actionLink"/>
-      </p>
-    </div>
-    <div class="features" v-if="data.features && data.features.length">
-      <div class="feature" v-for="feature in data.features">
-        <h2>{{ feature.title }}</h2>
-        <p>{{ feature.details }}</p>
+    <div class="hero cdr-stack--xl">
+      <cdr-img
+        v-if="data.heroImage"
+        class="hero__image"
+        :src="$withBase(data.heroImage)"
+        ratio="2-1"
+        crop="top x-center"
+        cover
+        alt="hero"/>
+      <div class="hero__container cdr-text-center">
+        <cdr-text
+          tag="h1"
+          modifier="display"
+          class="cdr-stack"
+        >{{ data.heroTitle }}</cdr-text>
+        <p class="hero__description">{{ data.heroDescription }}</p>
       </div>
     </div>
-    <Content custom/>
-    <div class="footer" v-if="data.footer">
-      {{ data.footer }}
+
+
+    <div class="cdr-container">
+      <cdr-row cols="1 2@md" align="middle" class="cdr-stack--xl">
+        <cdr-col>
+          <cdr-img
+            class="getting-started-image"
+            :src="$withBase('hero-banner.jpg')" 
+            ratio="16-9"
+            cover
+            radius="rounded"
+          />
+        </cdr-col>
+        <cdr-col>
+          <div>
+            <h2 class="home-heading">Getting Started</h2>
+            <p class="home-subheading">Learn how to start using Cedar components with handy onboarding guides.</p>
+            <cdr-row cols="auto" justify="around">
+              <cdr-col>
+                <div>
+                  <cdr-button
+                    tag="a"
+                    :href="$withBase('/getting-started/designer')"
+                    modifier="secondary">View Designer Guide</cdr-button>
+                </div>
+              </cdr-col>
+              <cdr-col>
+                <div>
+                  <cdr-button
+                    tag="a"
+                    :href="$withBase('/getting-started/developer')"
+                    modifier="secondary">View Developer Guide</cdr-button>
+                </div>
+              </cdr-col>
+            </cdr-row>
+          </div>
+        </cdr-col>
+      </cdr-row>
+
+      <hr class="home-hr cdr-stack--xl"/>
+
+      <cdr-row cols="1 2@md" class="cdr-stack--xl">
+        <cdr-col>
+          <div class="cdr-text-center">
+            <cdr-img
+              class="home-start-image"
+              :src="$withBase('hero-banner.jpg')"
+              ratio="square"
+              radius="rounded"
+              cover
+            />
+            <h2 class="home-heading">Foundation</h2>
+            <cdr-text class="cdr-stack">Build cohesive digital experiences with the basic design elements that comprise the Cedar visual language.</cdr-text>
+            <cdr-link
+              :href="$withBase('/foundation/color')"
+              modifier="standalone"
+            >Start with color</cdr-link>
+          </div>
+        </cdr-col>
+        <cdr-col>
+          <div class="cdr-text-center">
+            <cdr-img
+              class="home-start-image"
+              :src="$withBase('hero-banner.jpg')"
+              ratio="square"
+              radius="rounded"
+              cover
+            />
+            <h2 class="home-heading">Components</h2>
+            <cdr-text class="cdr-stack">Use components as building blocks to create new digital products - usage, visual guidelines, and code included.</cdr-text>
+            <cdr-link
+              :href="$withBase('/components/button')"
+              modifier="standalone"
+            >Start with buttons</cdr-link>
+          </div>
+        </cdr-col>
+      </cdr-row>
+
+      <div class="home-hr-tree cdr-stack--xl">
+        <cdr-img :src="$withBase('hr_tree.png')" />
+      </div>
+
+
+      <h2 class="home-heading cdr-text-center cdr-stack--xl">Resources</h2>
+
+      <cdr-row cols="1 2@md" class="cdr-stack--xl">
+        <cdr-col>
+          <cdr-card class="cdr-inset--lg">
+            <cdr-row align="middle">
+              <cdr-col span="9">
+                <div>
+                  <cdr-text
+                    tag="h3"
+                    modifier="heading-small-static"
+                    class="cdr-stack"
+                  >Cedar Sketch UI toolkit</cdr-text>
+                  <cdr-link :href="$withBase('/getting-started/designer')" modifier="standalone">Get the Sketch library</cdr-link>
+                </div>
+              </cdr-col>
+              <cdr-col span="3">
+                <div>
+                  <cdr-img :src="$withBase('sketch_icon.png')" />
+                </div>
+              </cdr-col>
+            </cdr-row>
+          </cdr-card>
+        </cdr-col>
+        <cdr-col>
+          <cdr-card class="cdr-inset--lg">
+            <cdr-row align="middle">
+              <cdr-col span="9">
+                <div>
+                  <cdr-text
+                    tag="h3"
+                    modifier="heading-small-static"
+                    class="cdr-stack"
+                  >Vue.js components</cdr-text>
+                  <!-- TODO: link href -->
+                  <cdr-link href="" modifier="standalone">View the NPM repository</cdr-link>
+                </div>
+              </cdr-col>
+              <cdr-col span="3">
+                <div>
+                  <cdr-img :src="$withBase('vue_icon.png')" />
+                </div>
+              </cdr-col>
+            </cdr-row>
+          </cdr-card>
+        </cdr-col>
+        <cdr-col>
+          <cdr-card class="cdr-inset--lg">
+            <cdr-text
+              tag="h3"
+              modifier="heading-small-static"
+              class="cdr-stack"
+            >Contribute to Cedar</cdr-text>
+            <cdr-text class="cdr-stack">The Cedar team welcomes contributions from the community. Learn how to become a pilot contributor.  </cdr-text>
+            <cdr-link :href="$withBase('/adopting-cedar#contributions')" modifier="standalone">Help build Cedar</cdr-link>
+          </cdr-card>
+        </cdr-col>
+        <cdr-col>
+          <cdr-card class="cdr-inset--lg">
+            <cdr-text
+              tag="h3"
+              modifier="heading-small-static"
+              class="cdr-stack"
+            >Feedback & support</cdr-text>
+            <cdr-text class="cdr-stack">Questions, ideas, or comments? Your feedback can help improve Cedar. </cdr-text>
+            <cdr-link href="mailto:cedar@rei.com" modifier="standalone">Get in touch</cdr-link>
+          </cdr-card>
+        </cdr-col>
+      </cdr-row>
+
     </div>
+
+    <div class="home-adopt">
+      <h2 class="home-heading cdr-text-center">Looking to Adopt Cedar?</h2>
+      <p class="adopt-text">Cedar will grow and evolve as more teams adopt and reuse components. Check out our step-by-step guide to adopting Cedar and other helpful resources for teams to get started.</p>
+      <cdr-button tag="a" :href="$withBase('/getting-started/as-an-adopter/')" modifier="secondary">Get Cedar for your team</cdr-button>
+    </div>
+
+
+
+    <!-- <Content custom/> -->
   </div>
 </template>
 
@@ -43,4 +205,95 @@ export default {
 </script>
 
 <style lang="scss">
+@import "styles/cdr-tokens.scss";
+
+.hero {
+  position: relative;
+
+  &__image {
+    &::before {
+      padding-bottom: 40% !important;
+    }
+  }
+
+  &__container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    color: white; //TODO: replace with token
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  &__description {
+    @include redwood-display-40();
+    max-width: 400px;
+    margin: 0 auto;
+  }
+}
+
+.home-heading {
+  @include spruce-display-60();
+  margin-bottom: $space-1-x;
+}
+
+.home-subheading {
+  @include redwood-body-40();
+  margin-bottom: $space-1-x;
+}
+
+.getting-started-image {
+  max-width: 375px;
+  margin: 0 auto;
+}
+
+.home-start-image {
+  max-width: 170px;
+  margin: 0 auto $space-1-x;
+}
+
+.home-adopt {
+  background-color: #EFEEE1;
+  text-align: center;
+  padding: $space-2-x;
+}
+
+.home-hr {
+  color: black;
+}
+
+.home-hr-tree {
+  position: relative;
+  text-align: center;
+
+  &::before {
+    content: '';
+    height: 1px;
+    position: absolute;
+    top: 50%;
+    left: 0;
+    transform: translateY(-50%);
+    width: 33%;
+    border-top: 1px solid black;
+  }
+
+  &::after {
+    content: '';
+    height: 1px;
+    position: absolute;
+    top: 50%;
+    right: 0;
+    transform: translateY(-50%);
+    width: 33%;
+    border-top: 1px solid black;
+  }
+}
+
+.adopt-text {
+  max-width: 600px;
+  margin: 0 auto $space-2-x;
+}
 </style>
