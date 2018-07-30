df9271ca3d94ad44022f36ab0a9b7dced47006b3
Lance Harper <laharpe@rei.com>
2018-07-30 15:59:21 -0700

    Fix local nav scroll issues

---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,28 @@
+var _ = require('lodash');
+
+export default ({
+  Vue, // the version of Vue being used in the VuePress app
+  options, // the options for the root Vue instance
+  router, // the router instance for the app
+  siteData // site metadata
+}) => {
+  router.options.scrollBehavior = function(to, frm, savedPosition) {
+    if (frm && to.name === frm.name && _.isEqual(to.params, frm.params)) {
+      return;
+    }
+    // Handle scroll
+    // copied from https://github.com/vuejs/vuepress/blob/master/lib/app/app.js
+    if (saved) {
+      return saved
+    } else if (to.hash) {
+      if (store.disableScrollBehavior) {
+        return false
+      }
+      return {
+        selector: to.hash
+      }
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
+}
