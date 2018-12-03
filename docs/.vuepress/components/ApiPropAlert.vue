10ef3ad7b224b20e8e365fc7cde1da4dddf3888c
Darin Trimillos <darin@trimillos.com>
2018-12-03 14:58:32 -0800

    Added icons to ApiPropAlert

--- docs/.vuepress/components/ApiPropAlert.vue
+++ docs/.vuepress/components/ApiPropAlert.vue
@@ -1,14 +1,26 @@
 <template>
-  <div class="api-prop-alert">
+  <div class="api-prop-alert" :class="'alert-' + alert.type">
     <div class="api-prop-alert__message prop-description">
+      <icon-information-fill 
+        class="api-prop-alert-icon"
+        v-if="alert.type === 'updated'" />
+      <icon-warning-tri
+        class="api-prop-alert-icon"
+        v-if="alert.type === 'deprecated'" />
       {{ alert.description }}
     </div>
   </div>
 </template>
 
 <script>
+  import { IconInformationFill, IconWarningTri } from "@rei/cdr-icon";
+
   export default {
     name: 'ApiPropAlert',
+    components: {
+      IconInformationFill,
+      IconWarningTri
+    },
     props: {
       alert: {
         type: Object
@@ -19,14 +31,30 @@
 
 <style lang="scss">
   .api-prop-alert {
-    background-color: #3278ae;
-    border: 1px solid #3278ae;
     margin-bottom: 10px;
     padding-left: 5px;
 
     .api-prop-alert__message {
       background-color: #fff;
       padding: 5px 0 5px 5px;
+    }
+  }
+
+  .api-prop-alert.alert-updated {
+    background-color: #3278ae;
+    border: 1px solid #3278ae;
+
+    .api-prop-alert-icon {
+      fill: #3278ae;
+    }
+  }
+
+  .api-prop-alert.alert-deprecated {
+    background-color: #c77523;
+    border: 1px solid #c77523;
+
+    .api-prop-alert-icon {
+      fill: #c77523;
     }
   }
 
