0615a06b8e8cd1405363396797e6655556ff1587
Cole Willsea <cowills@rei.com>
2019-05-16 13:15:15 -0700

    refactor sandbox util, add index.html to handle font loading

--- utils/buildSandbox.js
+++ utils/buildSandbox.js
@@ -1,6 +1,6 @@
 import { getParameters } from 'codesandbox/lib/api/define';
 
-const INDEX_FILE = `import Vue from "vue";
+const INDEX_JS = `import Vue from "vue";
 import App from "./App";
 
 import "@rei/cedar/dist/cdr-fonts.css";
@@ -10,26 +10,25 @@ new Vue({
 el: "#app",
 components: { App },
 template: "<App/>"
-});`
+});`;
 
 export default function makeMeASandbox(data, model) {
-  // TODO: can we pull name from main page frontmatter?
   if(!data.components) return false
-  const fontImport = "";
-
+  // TODO: can we grab the preceding heading to use for name/title?
+  const name = "Cedar Example Sandbox";
 
   const parameters = {
     files: {
       'index.js': {
-        content: INDEX_FILE,
+        content: INDEX_JS,
       },
       'App.vue': {
-        content: `<template>\n<div style="margin: 32px;">\n\n${data.code}\n</div>\n</template>\n\n<script>\n${buildScriptTag(data, model)}\n</script>\n<style>\n${fontImport}\n${data.styleTag || ''}</style>`
+        content: buildContent(data, model)
       },
       'package.json': {
         content: {
-          // TODO: can we pull a sensible name/description from the surrounding docs somehow?
-          "name": "Cedar Example Sandbox",
+          "name": name,
+            // TODO: can we grab the preceding text to use for description?
           "description": "https://rei.github.io/rei-cedar-docs/",
           "dependencies": {
             "@rei/cedar": "^1.0.0-alpha.1",
@@ -38,15 +37,54 @@ export default function makeMeASandbox(data, model) {
           }
         },
       },
+      'index.html': {
+        content: buildIndexHtml(name)
+      }
     },
   };
 
   return `https://codesandbox.io/api/v1/sandboxes/define?parameters=${getParameters(parameters)}`;
 }
 
-function buildScriptTag(data, model) {
+function buildIndexHtml(title) {
+  return `<!DOCTYPE html>
+  <html>
 
-  return `import { ${data.components} } from "@rei/cedar";
+  <head>
+  	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+  	<title>${title}</title>
+  	<link href="https://www.rei.com/static/rev/global/head/rei-cedar-fontless-7478200331.min.css" />
+  	<link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed" rel="stylesheet">
+  </head>
+  
+  <body>
+  	<div id="app"></div>
+  </body>
+  
+  </html>`;
+}
+
+function buildContent(data, model, fontImport) {
+  return `
+<template>
+  <div style="margin: 32px;">
+    ${data.code}
+  </div>
+</template>
+
+<script>
+  ${buildScriptTag(data, model)}
+</script>
+
+<style>
+  ${data.styleTag || ''}
+</style>`;
+}
+
+function buildScriptTag(data, model) {
+  return `
+import { ${data.components} } from "@rei/cedar";
 
 export default {
   name: "App",
