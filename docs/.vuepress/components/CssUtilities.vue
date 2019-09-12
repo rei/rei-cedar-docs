eaea182f644b8c130592987a6ed5431d61968406
Lance Harper <laharpe@rei.com>
2019-09-12 09:32:52 -0700

    Consume css utility data from cedar package

--- docs/.vuepress/components/CssUtilities.vue
+++ docs/.vuepress/components/CssUtilities.vue
@@ -40,8 +40,7 @@
 </template>
 
 <script>
-// TODO: get this from @rei/cedar package when available -- hardcoded here for now
-import cssData from './cssData.json';
+import cssData from '@rei/cedar/dist/cssdocdata.json';
 
 export default {
   name: 'css-utilities',
