7eab1ebe42e596d657c156e39df52385007ec538
Lance Harper <laharpe@rei.com>
2019-09-13 10:58:14 -0700

    Fix TOC not loading sometimes when changing pages

--- docs/.vuepress/components/cdr-doc-table-of-contents-shell.vue
+++ docs/.vuepress/components/cdr-doc-table-of-contents-shell.vue
@@ -6,14 +6,17 @@
     <div class="cdr-doc-table-of-contents-shell__navigation">
       <cdr-doc-local-anchor-nav
         :tab-name="tabName"
-        :parent-selectors="'.cdr-doc-table-of-contents-shell--' + instanceId + ' .cdr-doc-table-of-contents-shell__content h2'" 
-        :child-selectors="'.cdr-doc-table-of-contents-shell--' + instanceId + ' .cdr-doc-table-of-contents-shell__content h3'"
+        :parent-selectors="parentSelectors"
+        :child-selectors="childSelectors"
+        :links="links"
         :appended-items="appendedNavItems"/>
     </div>
   </div>
 </template>
 
 <script>
+import slugify from '../../../utils/slugify.js';
+
 export default {
   name: 'CdrDocTableOfContentsShell',
   props: {
@@ -28,13 +31,89 @@ export default {
       default: false
     }
   },
-  data: function() {
+  data() {
     return {
-      instanceId: null
+      instanceId: null,
+      links: [
+        {
+          href: '#',
+          text: 'Click Me'
+        }
+      ],
     }
   },
-  beforeMount: function() {
+  computed: {
+    parentSelectors() {
+      return `.cdr-doc-table-of-contents-shell--${this.instanceId} .cdr-doc-table-of-contents-shell__content h2`
+    },
+    childSelectors() {
+      return `.cdr-doc-table-of-contents-shell--${this.instanceId} .cdr-doc-table-of-contents-shell__content h3`
+    },
+  },
+  beforeMount() {
     this.instanceId = this.instanceId || this._uid;
+  },
+  mounted() {
+    this.createAnchorsFromContent();
+
+    // So child elements can tell it to update toc
+    // Fixes issue with rendering children that have h2/h3 not in markdown
+    // Use this.$parent.$emit('update-toc');
+    // See CssUtilities.vue for example (in mounted())
+    this.$on('update-toc', () => {
+      this.createAnchorsFromContent()
+    });
+  },
+  methods: {
+    createAnchorsFromContent () {
+      // Polyfill matches() for IE11
+      if (!Element.prototype.matches) {
+          Element.prototype.matches = Element.prototype.msMatchesSelector;
+      }
+
+      const selectors = `${this.parentSelectors}, ${this.childSelectors}`;
+      const anchorElements = document.querySelectorAll(selectors);
+      const links = [];
+      const anchorIds = [];
+
+      for (let i=0; i < anchorElements.length; i++) { // Traditional for loop to support IE 11
+        const anchorElement = anchorElements[i];
+        let anchorId = anchorElement.getAttribute('id');
+        const text = anchorElement.textContent.replace(/^# /, '');
+        // Markdown in VuePress automatically creates anchor links within headings with a '#' symbol as the text, strip this out of the textContent
+        // Vuepress header markup: <h1 id="automatically-created-by-vuepress"><a href="#automatically-created-by-vuepress">#</a>Automatically Created By Vuepress</h1>
+        if (anchorId === null) {
+          // If the Dom element doesn't already have an id, create one from its textContent and set it
+          anchorId = slugify(text);
+        }
+
+        // If an anchor starts with a number, prefix it with an underscore (_) to make it more compatible with CSS
+        if (anchorId.match(/^[0-9]+/)) {
+          anchorId = `_${anchorId}`;
+        }
+
+        // If an ID is repeated on the page, append a numeric index to it, so two anchor elements of "Guidelines" will result in #guidelines, and #guidelines-2 ids/anchors being created
+        if (anchorIds.indexOf(anchorId) !== -1) {
+          const existingIdCount = anchorIds.filter(idString => idString.replace(/-[0-9]*$/, '') === anchorId).length;
+          anchorId = `${anchorId}-${(existingIdCount + 1)}`;
+        }
+
+
+        anchorElement.setAttribute('id', anchorId);
+        const linkData = {
+          href: `#${anchorId}`,
+          text: text
+        };
+
+        if (anchorElement.matches(this.childSelectors)) {
+          linkData.isChild = true;
+        }
+
+        anchorIds.push(anchorId);
+        links.push(linkData)
+      }
+      this.links = links;
+    },
   }
 }
 </script>
