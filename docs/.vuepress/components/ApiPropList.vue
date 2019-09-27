9272004ea2a50e0c56cded8dcf87be10fd7203e1
michael <mhewson@gmail.com>
2019-09-27 10:38:02 -0700

    getting this list working

--- docs/.vuepress/components/ApiPropList.vue
+++ docs/.vuepress/components/ApiPropList.vue
@@ -1,10 +1,7 @@
 <template>
-  <cdr-list modifier="modifier="unordered"">
+  <cdr-list modifier="unordered"">
     <li v-for="item in items" :key="item.id">
-    <!-- We have a slot for each item, passing it the -->
-    <!-- `item` object as a slot prop.                 -->
       <slot v-bind:item="item">
-        <!-- Fallback content -->
         {{ item }}
       </slot>
     </li>
@@ -15,11 +12,6 @@
 export default {
   name: "List",
   props: {
-    tag: {
-      type: String,
-      default: "ul",
-      validator: value => ["ul", "ol"].indexOf(value) >= 0 || false
-    },
     items: {
       type: Array,
       default: () => []
@@ -28,9 +20,6 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.list--unstyled {
-  list-style: none;
-}
+
 </style>
