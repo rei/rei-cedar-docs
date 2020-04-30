7954e8386e4a3e54f332a50e15675bf0b98a3aaa
Cole Willsea <cowills@rei.com>
2020-04-30 11:55:45 -0700

    remove un-used layout logic

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="theme-container"
-    :class="pageClasses"
-    @touchstart="onTouchStart"
-    @touchend="onTouchEnd">
+    :class="pageClasses">
     <div v-html="iconSprite" style="display: none"/>
     <div class="cdr-doc-page-shell">
       <div class="cdr-doc-page-shell__side-navigation">
         <div class="cdr-doc-side-navigation">
-          <Navbar v-if="shouldShowNavbar" />
+          <Navbar/>
         </div>
       </div>
       <div class="cdr-doc-page-shell__body">
@@ -23,7 +21,6 @@
 
 <script>
 import Vue from 'vue'
-import nprogress from 'nprogress'
 import iconSprite from '@rei/cedar-icons/dist/all-icons.svg';
 import BackToTopBtn from './BackToTop.js';
 import Home from './Home.vue'
@@ -34,31 +31,14 @@ export default {
   components: { Home, Navbar, BackToTopBtn },
   data () {
     return {
-      isSidebarOpen: false,
       iconSprite
     }
   },
 
   computed: {
-    shouldShowNavbar () {
-      const { themeConfig } = this.$site
-      const { frontmatter } = this.$page
-      if (frontmatter.navbar === false) return false
-      return (
-        this.$title ||
-        themeConfig.logo ||
-        themeConfig.repo ||
-        themeConfig.nav ||
-        this.$themeLocaleConfig.nav
-      )
-    },
     pageClasses() {
       const userPageClass = this.$page.frontmatter.pageClass
       return [
-        {
-          'no-navbar': !this.shouldShowNavbar,
-          'sidebar-open': this.isSidebarOpen,
-        },
         userPageClass
       ]
     }
@@ -78,20 +58,6 @@ export default {
     this.currentMetaTags = []
     this.updateMeta()
 
-    // configure progress bar
-    nprogress.configure({ showSpinner: false })
-
-    this.$router.beforeEach((to, from, next) => {
-      if (to.path !== from.path && !Vue.component(to.name)) {
-        nprogress.start()
-      }
-      next()
-    })
-
-    this.$router.afterEach(() => {
-      nprogress.done()
-      this.isSidebarOpen = false
-    })
   },
 
   beforeDestroy () {
@@ -131,27 +97,6 @@ export default {
         })
       }
     },
-    toggleSidebar (to) {
-      this.isSidebarOpen = typeof to === 'boolean' ? to : !this.isSidebarOpen
-    },
-    // side swipe
-    onTouchStart (e) {
-      this.touchStart = {
-        x: e.changedTouches[0].clientX,
-        y: e.changedTouches[0].clientY
-      }
-    },
-    onTouchEnd (e) {
-      const dx = e.changedTouches[0].clientX - this.touchStart.x
-      const dy = e.changedTouches[0].clientY - this.touchStart.y
-      if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
-        if (dx > 0 && this.touchStart.x <= 80) {
-          this.toggleSidebar(true)
-        } else {
-          this.toggleSidebar(false)
-        }
-      }
-    }
   }
 }
 
