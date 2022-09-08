a6ef476dd87d82e35ef6ddccdbf7b5287c433898
Ben Griffin <grif88@gmail.com>
2022-09-08 09:56:10 -0700

    Plural for consistency

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -116,7 +116,7 @@ module.exports = {
           { text: "Tabs", link: "/components/tabs/" },
           { text: "Text", link: "/components/text/" },
           { text: "Toast", link: "/components/toast/" },
-          { text: "Toggle Button", link: "/components/toggle-button/" },
+          { text: "Toggle Buttons", link: "/components/toggle-button/" },
           { text: "Tooltip", link: "/components/tooltip/" }
         ]
       },
